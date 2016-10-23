7e8fdb5d9f950382b3bac52b85561a139cbc6cff
Diachkova Nadia <alphaomega@rambler.ru>
2016-10-23 21:47:54 +0300

    Добавлено несколько функций

--- Sandbox/src/main/java/home/one/myfirstprogram.java
+++ Sandbox/src/main/java/home/one/myfirstprogram.java
@@ -3,7 +3,25 @@ package home.one;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+    hello("world");
+    hello("Andrey");
+    hello("Nadia");
 
-		System.out.println("Hello, world!");
-	}
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+  public static void hello(String something) {
+		System.out.println("Hello, " + something + "!");
+
+   }
+   public static double area(double len){
+     return len*len;
+   }
+   public static double area(double a, double b){
+     return a*b;
+   }
 }
