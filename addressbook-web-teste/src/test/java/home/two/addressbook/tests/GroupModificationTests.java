d0bee13ac574e648befcf6e500cb18abe1f41312
Diachkova Nadia <alphaomega@rambler.ru>
2016-10-31 04:22:11 +0300

    Добавлен тест изменения группы

---
+++ addressbook-web-teste/src/test/java/home/two/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package home.two.addressbook.tests;
+
+import home.two.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Andrey on 31.10.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupMofification();
+    app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
