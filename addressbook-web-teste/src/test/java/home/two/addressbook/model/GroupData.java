7b6cf366ac62aa104cb81f43faa1c4fe1181c342
Diachkova Nadia <alphaomega@rambler.ru>
2016-11-13 18:02:52 +0300

    Группы работают на сравнение

--- addressbook-web-teste/src/test/java/home/two/addressbook/model/GroupData.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/model/GroupData.java
@@ -1,14 +1,27 @@
 package home.two.addressbook.model;
 
 public class GroupData {
+  private final String id;
   private final String groupName;
   private final String header;
   private final String footer;
+
+  public String getId() {
+    return id;
+  }
+
+  public GroupData(String id, String groupName, String header, String footer) {
+    this.groupName = groupName;
+    this.header = header;
+    this.footer = footer;
+    this.id = id;
+  }
 
   public GroupData(String groupName, String header, String footer) {
     this.groupName = groupName;
     this.header = header;
     this.footer = footer;
+    this.id = null;
   }
 
   public String getGroupName() {
@@ -30,20 +43,23 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
     return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
 
   }
 
   @Override
   public int hashCode() {
-    return groupName != null ? groupName.hashCode() : 0;
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
+    return result;
   }
 
   @Override
-
   public String toString() {
     return "GroupData{" +
-            "groupName='" + groupName + '\'' +
+            "id='" + id + '\'' +
+            ", groupName='" + groupName + '\'' +
             '}';
   }
 }
--- addressbook-web-teste/src/test/java/home/two/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import home.two.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -19,11 +20,17 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test3", null, null));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(),
+            "Test3", null, null);
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 }
