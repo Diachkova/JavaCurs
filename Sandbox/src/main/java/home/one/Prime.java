5573da21040cfaad2ff0c98e60404b61ea5033ba
Diachkova Nadia <alphaomega@rambler.ru>
2016-11-13 10:09:15 +0300

    конструкция while

--- Sandbox/src/main/java/home/one/Prime.java
+++ Sandbox/src/main/java/home/one/Prime.java
@@ -5,10 +5,18 @@ public class Prime {
 
   public static boolean isPrime(int n) {
     for (int i = 2; i < n; i++) {
-        if (n %  i == 0) {
+      if (n %  i == 0) {
         return false;
       }
     }
     return  true;
+  }
+  public static boolean isPrimeWhile(int n) {
+    int i = 2;
+    while (i < n && n % i == 0 ) {
+      i++;
+    }
+
+    return  i == n;
   }
 }
