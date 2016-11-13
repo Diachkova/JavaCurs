d142a6d01f48eb236f284f1e0e08a102be16dd6d
Diachkova Nadia <alphaomega@rambler.ru>
2016-11-13 11:36:22 +0300

    объявление массива

---
+++ Sandbox/src/main/java/home/one/Collections.java
@@ -0,0 +1,14 @@
+package home.one;
+
+/**
+ * Created by Andrey on 13.11.2016.
+ */
+public class Collections {
+  public  static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    for (int i = 0; i < langs.length; i++) {
+      System.out.println(" я хочу выучить " + langs[i]);
+    }
+  }
+}
