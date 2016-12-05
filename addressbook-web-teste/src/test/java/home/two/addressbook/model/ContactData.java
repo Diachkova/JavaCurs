d7a6e6746574c88b7fe4afd1c8479a456e2dac5a
Nardarika <n.dyachkova@mangotele.com>
2016-12-05 19:27:52 +0300

    начало работы с файлами

--- addressbook-web-teste/src/test/java/home/two/addressbook/model/ContactData.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package home.two.addressbook.model;
 
+import java.io.File;
+
 public class ContactData {
 
   private int id = Integer.MAX_VALUE;;
@@ -22,6 +24,17 @@ public class ContactData {
   private String allAddress;
   private String allEmail;
   private String allData;
+
+  public File getPhoto() {
+    return photo;
+  }
+
+  public ContactData withPhoto(File photo) {
+    this.photo = photo;
+    return this;
+  }
+
+  private File photo;
 
   public String getAllName() {
     return allName;
--- addressbook-web-teste/src/test/java/home/two/addressbook/tests/ContactCreation.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/tests/ContactCreation.java
@@ -4,24 +4,33 @@ import home.two.addressbook.model.ContactData;
 import home.two.addressbook.model.Contacts;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreation extends TestBase {
 
-  @Test
+  @Test(enabled = false)
   public void testContactCreation() {
     app.goTo().contactPage();
     Contacts before = app.contact().allC();
     System.out.println("before " + before.size());
+    File photo = new File("src/test/resourses/stru.png");
     ContactData contact = new ContactData().withName("Nadia").withMiddleName("Yurievna").
-            withSurname("Diachkova").withNik("Nicki").withAddress("MyOwn").
-            withHome("111").withEmail("terra72@inbox.ru").withGroup("Test3");
+            withSurname("Diachkova").withAddress("MyOwn").
+            withHome("111").withEmail("terra72@inbox.ru").withPhoto(photo);
     app.contact().createContact(contact);
     assertThat(app.contact().count(), equalTo(before.size()+1));
     Contacts after = app.contact().allC();
     System.out.println("after " + after.size());
     assertThat(after, equalTo(
             before.withCAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+  }
+
+  @Test
+  public void testCurrentDir() {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
   }
 }
