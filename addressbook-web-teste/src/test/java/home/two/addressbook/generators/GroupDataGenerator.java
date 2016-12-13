d9f6da5d8514ae583a6d6d83a9732e348ac5ff47
Nardarika <n.dyachkova@mangotele.com>
2016-12-13 20:59:17 +0300

    Создание групп с параметрами

--- addressbook-web-teste/src/test/java/home/two/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/generators/GroupDataGenerator.java
@@ -1,6 +1,10 @@
 package home.two.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import home.two.addressbook.model.GroupData;
+import org.junit.runners.Parameterized;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,15 +17,33 @@ import java.util.List;
  * Created by ndya on 06.12.2016.
  */
 public class GroupDataGenerator {
-  public static void main(String[] args) throws IOException {
-   int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
 
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  public void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+
+  private void save(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n", group.getGroupName(), group.getHeader(), group.getFooter()));
@@ -29,7 +51,7 @@ public class GroupDataGenerator {
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withGroupName(String.format("test%s", i)).withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
--- addressbook-web-teste/src/test/java/home/two/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/tests/GroupCreationTests.java
@@ -2,20 +2,35 @@ package home.two.addressbook.tests;
 
 import home.two.addressbook.model.GroupData;
 import home.two.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static java.util.List.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    ArrayList<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {"test1", "header1", "footer1"});
+    list.add(new Object[] {"test2", "header2", "footer2"});
+    list.add(new Object[] {"test3", "header3", "footer3"});
+    return list.iterator();
+  }
 
-  @Test
-  public void testGroupCreation() {
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(String name, String header, String footer) {
+    GroupData group = new GroupData().withGroupName(name).withHeader(header).withFooter(footer);
     app.goTo().groupPage();
     Groups before = app.group().all();
     System.out.println("before " + before);
-    GroupData group = new GroupData().withGroupName("Test1");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()+1));
     Groups after = app.group().all();
