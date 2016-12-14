e3007e7c8c630e186a28dd9ebfb352c928554504
Nardarika <n.dyachkova@mangotele.com>
2016-12-14 20:08:28 +0300

    Генерация и чтение данных в группах json и xml

--- addressbook-web-teste/src/test/java/home/two/addressbook/model/GroupData.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/model/GroupData.java
@@ -1,9 +1,18 @@
 package home.two.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+@XStreamAlias("group")
 public class GroupData {
-  private int id = Integer.MAX_VALUE;;
+  @XStreamOmitField
+  private int id = Integer.MAX_VALUE;
+  @Expose
   private  String groupName;
+  @Expose
   private  String header;
+  @Expose
   private  String footer;
 
   public GroupData withId(int id) {
