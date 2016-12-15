fc076597c72f7036adf8c9a012e76be18e573cf2
Nardarika <n.dyachkova@mangotele.com>
2016-12-15 21:04:53 +0300

    пока не работает TestBase

--- addressbook-web-teste/src/test/java/home/two/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-teste/src/test/java/home/two/addressbook/appmanager/ApplicationManager.java
@@ -6,11 +6,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
 public class ApplicationManager {
+  private final Properties properties;
   public WebDriver wd;
 
   private ContactHelper contactHelper;
@@ -19,16 +25,19 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
+    properties = new Properties();
+
   }
 
-  public void init() {
+  public void init() throws IOException {
     //System.setProperty("webdriver.chrome.driver", "C:\\Nadia\\driver\\chromedriver_win32\\chromedriver.exe");
     //System.setProperty("webdriver.gecko.driver", "C:\\Nadia\\driver\\geckodriver-v0.11.1-win64\\geckodriver.exe");
     //System.setProperty("webdriver.ie.driver", "C:\\Nadia\\driver\\IEDriverServer_Win32_2.53.1\\IEDriverServer.exe");
     //System.setProperty("webdriver.opera.driver", "C:\\Nadia\\driver\\operadriver_win32\\operadriver.exe");
-
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
     if (Objects.equals(browser, BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
@@ -39,12 +48,13 @@ public class ApplicationManager {
     }
 
     wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd, this);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd, this);
     sessionHelper = new SessionHelper(wd);
-    wd.get("http://localhost/addressbook/");
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
   }
 
   public void stop() {
