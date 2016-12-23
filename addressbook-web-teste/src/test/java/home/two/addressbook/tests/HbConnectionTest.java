f4b31719ec7f445efcf288000a3a4d902358245d
Nardarika <n.dyachkova@mangotele.com>
2016-12-23 19:54:44 +0300

    подключаем hb

---
+++ addressbook-web-teste/src/test/java/home/two/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,13 @@
+package home.two.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by ndya on 23.12.2016.
+ */
+public class HbConnectionTest {
+  @Test
+  public void testHbConnection() {
+
+  }
+}
